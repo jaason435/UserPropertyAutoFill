9c0bb972174d074945d7958e7acc045d08c4dfbb
jaason435 <jaason435@gmail.com>
2018-08-08 16:41:12 -0400

    Reduced to a single call, clears fields if user is deleted

--- UserProfile.js
+++ UserProfile.js
@@ -2,60 +2,72 @@
 var params = {
 	userField: 'Employee_x0020_Name', 
 	properties: {
-		'Title': 'Job Title',
-		'SPS-Location': 'Region',
-		'Office': 'Location',
-        'SubFunction': 'Sub Function',
-        'UserName': 'Employee ID',
-        'Region': 'Region'
+		Title: 'Job Title',
+		SPS-Location: 'Region',
+		Office: 'Location',
+        SubFunction: 'Sub Function',
+        UserName: 'Employee ID',
+        Region: 'Region'
 	}
 };*/
 
-function run(params) {
-	console.log('running');
-	var properties = buildPropertyArray();
+function startUserAutoFill(params) {
+	var propertiesNeeded = buildPropertyArray(params);
 	var userFieldSelecter = "input[id^='"+params.userField+"'], input[id^='"+params.userField+" Required Field']";	
 	$(document).on("focusout", userFieldSelecter, function() {
 		var userID = getUserID(userFieldSelecter);
 		if (userID != null) {
-			var promises = [];
-			for (i=0; i<properties.length; i++) { 
-				var property = properties[i];
-				promises.push(getProperty(property, userID));
-			}
-			Promise.all(promises);
+			getPropertiesAndPopulate(userID, propertiesNeeded, params);
 		}
-		// inside here I need another event listener for when the value changes
+		else {
+			clear(params, propertiesNeeded);
+		}
 	});
 }
 
-function getProperty(property, userID) {
-	return new Promise(function(resolve, reject) {
-		$.ajax({
-			url: _spPageContextInfo.webAbsoluteUrl + "/_api/SP.UserProfiles.PeopleManager/GetUserProfilePropertyFor(accountName=@v,propertyName='" + property + "')?@v='" + userID + "'",
-			type: "GET",
-			headers: { "accept": "application/json;odata=verbose" },
-			success: function (result) {
-			   var fieldID = params.properties[property].replace(" ", "_x0020_");
-			   var value = result.d.GetUserProfilePropertyFor;
-			   setFieldValue(fieldID, value);
-			   Promise.resolve('success');
-			},
-			error: function(error) {
-				console.log(error);	
-				Promise.reject('error');
-			} 
-		});
-	});
+function getPropertiesAndPopulate(username, propertiesNeeded, params) {
+	var url = _spPageContextInfo.webServerRelativeUrl + "/_api/SP.UserProfiles.PeopleManager/GetPropertiesFor(accountName=@v)?@v='" + encodeURIComponent(username) + "'&$select=UserProfileProperties";
+	$.ajax({ 
+		url : url, 
+	    type : "GET", 
+	    headers: { "accept": "application/json;odata=verbose" }, 
+	    success:function(data) 
+	    { 	
+	    	var allProperties = data.d.UserProfileProperties.results;
+	    	populate(allProperties, propertiesNeeded, params);
+	    }, 
+	    error:function(jqxr,errorCode,errorThrown)
+	    { 
+	    	console.log('An error occurred while retrieving user properties');
+	    } 
+	});	
+}
+
+function populate(allUserProperties, propertiesToFetch, params) {
+	for (i=0; i<allUserProperties.length; i++) { 
+		var property = allUserProperties[i].Key;
+		// check if property is specified in params
+		if (propertiesToFetch.includes(property)) {
+			var value = allUserProperties[i].Value;
+			var fieldID = params.properties[property].replace(" ", "_x0020_");
+			setFieldValue(fieldID, value);
+		}
+	}
+}
+
+function clear(params, propertiesToFetch) {
+	for(i=0; i < propertiesToFetch.length; i++) {
+		var property = propertiesToFetch[i];
+		var fieldID = params.properties[property].replace(" ", "_x0020_");
+		setFieldValue(fieldID, '');
+	}
 }
 
 function setFieldValue(fieldID, value) {
 	try {
 		var selecter = "input[id^='"+fieldID+"'], input[id^='"+fieldID+" Required Field']";	
 		var currentValue = document.querySelector(selecter).getAttribute('value');
-		if (currentValue == '') {
-			document.querySelector(selecter).setAttribute('value', value);
-		}
+		document.querySelector(selecter).setAttribute('value', value);
 	}
 	catch (err) {
 		console.log('Error reading property of field containing ' + fieldID + ' in its ID');
@@ -63,12 +75,13 @@ function setFieldValue(fieldID, value) {
 	
 }
 
-function buildPropertyArray() {
+function buildPropertyArray(params) {
 	var properties = [];
 	for (i=0; i < Object.keys(params.properties).length; i++) {
-		var key = Object.keys(params.properties)[i]
-		if (params.properties[key] != null) {	
-			properties.push(key.replace(" ", "_x0020_"));
+		var key = Object.keys(params.properties)[i];
+		var value = params.properties[key];
+		if (value != null) {
+			properties.push(key);
 		}
 	}
 	return properties;
@@ -87,7 +100,6 @@ function getUserID(selecter) {
 		}	
 	}
 	catch(err) {
-		console.log(err);
 		return null;
 	}
 }   
